20a77bf6a7e07bf5a6d9a42c52a35ec432ce209a
Ankita Kumari <ankita00iv@gmail.com>
2024-03-10 23:22:39 +0530

    initial commit

---
+++ Encapsulation.java
@@ -0,0 +1,28 @@
+class human {
+    private int age = 18;
+    private String name = "Ankita";
+
+    public String getname() {
+        return name;
+    }
+
+    public int getage() {
+        return age;
+    }
+
+    public void setage(int a) {
+        age = a;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        human obj = new human();
+        obj.setage(19);
+
+        // obj.age = 11;
+        // obj.name = "Ankita";
+
+        System.out.println(obj.getname() + ":" + obj.getage());
+    }
+}
