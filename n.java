3ba90a1fad913dec662b016bcab5f1f6469f4c32
Ankita Kumari <ankita00iv@gmail.com>
2024-03-15 22:46:16 +0530

    initial commit

---
+++ n.java
@@ -0,0 +1,37 @@
+class human {
+    private int age;
+    private String name;
+
+    public int getage() {
+        return age;
+    }
+
+    public void setage(int age, human obj) {
+
+        this.age = age;
+        // human obj1 = new human();
+        // obj1.age = age;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String n) {
+        name = n;
+    }
+
+}
+
+public class n {
+    public static void main(String[] args) {
+        human obj = new human();
+        obj.setage(19, obj);
+        obj.setname("Ankita");
+
+        // obj.age = 11;
+        // obj.name = "Ankita";
+
+        System.out.println(obj.getname() + ":" + obj.getage());
+    }
+}
