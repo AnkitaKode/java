39dd8b61e9e48f9bc430a929be50d87628180440
Ankita Kumari <ankita00iv@gmail.com>
2024-04-04 23:45:37 +0530

    initial commit

---
+++ AccessModifier.java
@@ -0,0 +1,15 @@
+class A {
+    private int data = 100;
+
+    private void msg() {
+        System.out.println("Hello java");
+    }
+}
+
+public class AccessModifier {
+    public static void main(String args[]) {
+        A obj = new A();
+        System.out.println(obj.data);// Compile Time Error
+        obj.msg(); // Compile Time Error
+    }
+}
