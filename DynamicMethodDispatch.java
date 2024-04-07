afb51cf19b121020210e86f2995412f395d76fd4
Ankita Kumari <ankita00iv@gmail.com>
2024-04-07 23:30:32 +0530

    initial commit

---
+++ DynamicMethodDispatch.java
@@ -0,0 +1,32 @@
+
+class A {
+    public void show() {
+        System.out.println("in A show");
+    }
+}
+
+class B extends A {
+    public void show() {
+        System.out.println("in B show");
+    }
+}
+
+class C extends A {
+    public void show() {
+        System.out.println("in C show");
+    }
+}
+
+public class DynamicMethodDispatch {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+
+        obj = new B();
+        obj.show();
+
+        obj = new C();
+        obj.show();
+
+    }
+}
---
+++ FinalKeyword.java
@@ -0,0 +1,6 @@
+public class FinalKeyword {
+    public static void main(String[] args) {
+        final int num = 80; // like constant in C ,final class()
+        System.out.println(num);
+    }
+}
