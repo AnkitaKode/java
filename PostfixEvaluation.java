39b94141954415a84fe3c3309863285b1e8f61f9
Ankita Kumari <ankita00iv@gmail.com>
2024-04-27 23:56:02 +0530

    initial commit

---
+++ PostfixEvaluation.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class PostfixEvaluation {
+
+    static int evaluatePostfix(String exp) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+
+            if (Character.isDigit(c))
+                stack.push(c - '0');
+            else {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+
+                switch (c) {
+                    case '+':
+                        stack.push(val2 + val1);
+                        break;
+                    case '-':
+                        stack.push(val2 - val1);
+                        break;
+                    case '/':
+                        stack.push(val2 / val1);
+                        break;
+                    case '*':
+                        stack.push(val2 * val1);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String exp = "231*+9-";
+
+        System.out.println("Postfix evaluation result: " + evaluatePostfix(exp));
+    }
+}
