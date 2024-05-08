9fab54d9922b18cd6f9326cd3e2f1a57c03d2b79
Ankita Kumari <ankita00iv@gmail.com>
2024-05-08 22:27:28 +0530

    initial commit

---
+++ Grid.java
@@ -0,0 +1,12 @@
+public class Grid {
+    public static void main(String[] args) {
+        int[][] a = new int[10][10];
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("%2d ", a[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
