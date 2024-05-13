1dc94bec8ad146d452eecf22eb0445a01c3a5f1e
Ankita Kumari <ankita00iv@gmail.com>
2024-05-13 22:23:53 +0530

    initial commit

---
+++ Game_Guess.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game_Guess {
+    private static final Random r = new Random();
+    private static final int NUMBER = r.nextInt(100) + 1;
+    private static int guess = 0;
+
+    public static void main(String[] args) {
+        Scanner player = new Scanner(System.in);
+        // System.out.println("number is " + String.valueOf(NUMBER)); //DEBUG
+        while (guess != NUMBER) {
+            // prompt player for guess
+            System.out.println("Guess a number between 1 and 100");
+            guess = player.nextInt();
+            if (guess > NUMBER) {
+                System.out.println("Guess a Lower number");
+            } else if (guess < NUMBER) {
+                System.out.println("Guess a Higher number");
+            } else {
+                System.out.println("That's right!");
+                System.exit(0);
+            }
+        }
+    }
+}
