51d103e9d114e5ec14e47432a6cf1e285d324828
Ankita Kumari <ankita00iv@gmail.com>
2024-05-14 22:15:01 +0530

    initial commit

---
+++ ASCII.java
@@ -0,0 +1,13 @@
+public class ASCII {
+
+    public static void main(String[] args) {
+        char ch = 'u';
+        int ascii = ch;
+
+        System.out.println("The ASCII value of character '" + ch + "' is: " + ascii);
+
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of character '" + ch + "' is: " + castAscii);
+    }
+}
