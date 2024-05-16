69cf5bb46d4028f622a5f06e11d51e62d83d92e7
Ankita Kumari <ankita00iv@gmail.com>
2024-05-16 22:30:48 +0530

    initial commit

---
+++ Maths.java
@@ -0,0 +1,24 @@
+public class Maths {
+    public static void main(String[] args) {
+        double x = 28;
+        double y = 4;
+
+        System.out.println("Maximum number of x and y is: " + Math.max(x, y));
+
+        System.out.println("Square root of y is: " + Math.sqrt(y));
+
+        System.out.println("Power of x and y is: " + Math.pow(x, y));
+
+        System.out.println("Logarithm of x is: " + Math.log(x));
+        System.out.println("Logarithm of y is: " + Math.log(y));
+
+        System.out.println("log10 of x is: " + Math.log10(x));
+        System.out.println("log10 of y is: " + Math.log10(y));
+
+        System.out.println("log1p of x is: " + Math.log1p(x));
+
+        System.out.println("exp of x is: " + Math.exp(x));
+
+        System.out.println("expm1 of x is: " + Math.expm1(x));
+    }
+}
