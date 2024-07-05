b79c1a0d413cc2b581fd0973e7705fa028c5fe5f
Ankita Kumari <ankita00iv@gmail.com>
2024-07-05 22:25:26 +0530

    initial commit

---
+++ StrConcat.java
@@ -0,0 +1,12 @@
+public class StrConcat {
+    public static void main(String args[]) {
+        String s = "Sachin";
+        s.concat(" Tendulkar");// concat() method appends the string at the end
+
+        System.out.println(s);// will print Sachin because strings are immutable objects
+
+        s = s.concat(" Tendulkar");
+
+        System.out.println(s);
+    }
+}
