1c578eb74dc57db42063d45d9f768e5604e7d00b
Ankita Kumari <ankita00iv@gmail.com>
2024-12-02 23:58:05 +0530

    initial commit

---
+++ ReverseArray.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+            start++;
+            end--;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        reverseArray(arr);
+
+        printArray(arr);
+        
+        scanner.close();
+    }
+}
