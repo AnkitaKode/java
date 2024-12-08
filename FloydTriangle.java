291e117034afa36cd2089b6ded47837c24445249
Ankita Kumari <ankita00iv@gmail.com>
2024-12-08 23:04:37 +0530

    initial commit

---
+++ FloydTriangle.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class FloydTriangle {
+    public static void main(String[] args) {
+        String s1="    hello string ";
+        System.out.println(s1+"Technolamror");//without trim()
+        System.out.println(s1.trim()+"Technolamror");
+    }
+}
