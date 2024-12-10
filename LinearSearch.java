c911313c2dd6f6f24ab1d4f58f5439a4a88947a3
Ankita Kumari <ankita00iv@gmail.com>
2024-12-10 23:21:45 +0530

    initial commit

---
+++ LinearSearch.java
@@ -0,0 +1,25 @@
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int target) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = { 10, 20, 30, 90, 50 };
+        int target = 30;
+
+        int result = linearSearch(numbers, target);
+
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index: " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array.");
+        }
+    }
+}
