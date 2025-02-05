549207fe8a3b37b2d043a8592e59d1a14582dfe0
Ankita Kumari <ankita00iv@gmail.com>
2025-02-05 23:38:47 +0530

    initial commit

---
+++ Return.java
@@ -0,0 +1,10 @@
+public class Return {
+    int display() {
+        return 99;
+    }
+
+    public static void main(String[] args) {
+        ReturnExample1 e = new ReturnExample1();
+        System.out.println(e.display());
+    }
+}
