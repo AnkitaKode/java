028faaa55da6d10dab5f76b49424a0e13d885f15
Ankita Kumari <ankita00iv@gmail.com>
2025-08-17 23:04:58 +0530

    initial commit

---
+++ Collections.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Collections {
+    public static void main(String[] args) {
+        
+       // Collection<Integer> nums = new ArrayList<Integer>(); //or just typecast as it is initially in object form
+
+        List<Integer> nums = new ArrayList<Integer>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(5);
+        nums.add(6);
+        nums.add(2);
+
+        for(int num:nums){
+            System.out.print(num+" ");
+        }
+
+        System.out.println(nums.get(1));
+        System.out.println(nums.indexOf(2));
+
+
+    }
+}
