39689dc3f4cec771b67c209ef0e82acaaf1d81db
Ankita Kumari <ankita00iv@gmail.com>
2026-02-15 13:29:29 +0530

    initial commit

--- BellamanFordAlgo.java
+++ BellamanFordAlgo.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
-
-// Bellman-Ford Algorithm is used to find the shortest path from a source vertex to all other vertices in a graph, even if the graph contains negative weight edges. It can also detect negative weight cycles in the graph. The algorithm works by relaxing all edges repeatedly for V-1 times, where V is the number of vertices in the graph. If we can still relax any edge after V-1 iterations, it means there is a negative weight cycle in the graph.
+// Negative weights + Detects negative cycle + Time Complexity: O(V * E) + O(E) for negative cycle check
+//The algorithm works by relaxing all edges repeatedly for V-1 times.If we can still relax any edge after V-1 iterations, it means there is a negative weight cycle in the graph.
 public class BellamanFordAlgo {
 
     static class Edge {
@@ -31,7 +31,7 @@ public class BellamanFordAlgo {
 
         graph[3].add(new Edge(3, 4, 4));
 
-        graph[4].add(new Edge(4, 1, -1));//-10 for negative cycle
+        graph[4].add(new Edge(4, 1, -1));// -10 for negative cycle
     }
 
     public static void bellamanFord(ArrayList<Edge> graph[], int src, int V) {
@@ -64,38 +64,37 @@ public class BellamanFordAlgo {
         }
 
         // negative cycle (nested code of bellamanFord)
-         for (int i = 0; i < V; i++) {
+        for (int i = 0; i < V; i++) {
 
-                for (int j = 0; j < graph[i].size(); j++) {
+            for (int j = 0; j < graph[i].size(); j++) {
 
-                    Edge e = graph[i].get(j);
-                    int u = e.source;
-                    int v = e.destination;
+                Edge e = graph[i].get(j);
+                int u = e.source;
+                int v = e.destination;
 
-                    if (dist[u] != Integer.MAX_VALUE &&
-                            dist[u] + e.weight < dist[v]) {
+                if (dist[u] != Integer.MAX_VALUE &&
+                        dist[u] + e.weight < dist[v]) {
 
-                        System.out.println("Negative Cycle Exists");
+                    System.out.println("Negative Cycle Exists");
 
-                    }
-                } 
+                }
             }
- 
+        }
 
         // // Step 3: Check Negative Cycle
         // for (int i = 0; i < V; i++) {
-        //     for (Edge e : graph[i]) {
+        // for (Edge e : graph[i]) {
 
-        //         int u = e.source;
-        //         int v = e.destination;
+        // int u = e.source;
+        // int v = e.destination;
 
-        //         if (dist[u] != Integer.MAX_VALUE &&
-        //                 dist[u] + e.weight < dist[v]) {
+        // if (dist[u] != Integer.MAX_VALUE &&
+        // dist[u] + e.weight < dist[v]) {
 
-        //             System.out.println("Negative Cycle Exists");
-        //             return;
-        //         }
-        //     }
+        // System.out.println("Negative Cycle Exists");
+        // return;
+        // }
+        // }
         // }
 
         // Step 4: Print shortest distances
